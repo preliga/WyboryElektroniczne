03265faf9b838bcf9baf2fb1c8bf790a989ec2db
Piotr Religa <piotr00it@gmail.com>
2018-03-02 19:03:13 +0100

    update submodułu

--- KomisjaWyborcza/public/content/elections.php
+++ KomisjaWyborcza/public/content/elections.php
@@ -11,23 +11,13 @@ use resource\action\Base;
 use resource\orm\templates\{
     Candidate
 };
+
 class elections extends Base
 {
     public function onAction()
     {
-        $sessionToken = $this->getParam('sessionToken');
-
-        $vote = \resource\orm\templates\Vote::getInstance()->findOne(['sessionToken = ?' => $sessionToken, 'NOW() < DATE_ADD(setSessionDateTime, INTERVAL 5 MINUTE)']);
-
-        if ($vote->empty()) {
-            $this->redirect(\library\PigFramework\model\Config::getInstance()->getConfig('AgencjaUprawnienURL'));
-        }
-
-        $this->view->sessionToken = $sessionToken;
-
         $candidates = Candidate::getInstance()->find();
 
         $this->view->candidates = $candidates;
     }
-
 }
